9fda6a078edab204bd80edc633b42cf2b92b7ff7
ChristopherHX <christopher.homberger@web.de>
2023-03-12 00:12:30 +0100

    Test

--- main.cpp
+++ main.cpp
@@ -18,12 +18,16 @@ uint32_t* c_get_memory(uint32_t size) {
         exit(-1);
     }
 
-    pthread_jit_write_protect_np(0);
-    sys_icache_invalidate(addr, size);
-
     return addr;
 }
 
 int main() {
-  printf("c_get_memory %d\n", (int)(intptr_t)c_get_memory(1024));
+  pthread_jit_write_protect_np(0);
+  auto mem = c_get_memory(1024);
+  printf("c_get_memory %d\n", (int)(intptr_t)mem);
+  mem[0] = 12;
+  mem[1] = 15;
+  printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
+  pthread_jit_write_protect_np(1);
+  sys_icache_invalidate(mem, 1024);
 }
