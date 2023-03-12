b4f138ee3137599c53b8b76cd7b3cf01c4d3838b
ChristopherHX <christopher.homberger@web.de>
2023-03-12 13:08:36 +0100

    .

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main() {
   sys_icache_invalidate(mem, 16384 * 10);
   printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
   pthread_jit_write_protect_np(0);
-  auto fixedaddr = (uint32_t*)mmap((char*)mem + 16384 * 5, 16384, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+  auto fixedaddr = (uint32_t*)mmap((char*)mem + 4096 * 5, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
   printf("fixedaddr %d\n", (int)(intptr_t)fixedaddr);
   pthread_jit_write_protect_np(1);
   sys_icache_invalidate(mem, 16384 * 10);
