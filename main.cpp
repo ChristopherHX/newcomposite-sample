fa1bc9bbb39048b14afb51eb1a1bbdee23846542
ChristopherHX <christopher.homberger@web.de>
2023-03-12 12:26:10 +0100

    test

--- main.cpp
+++ main.cpp
@@ -23,11 +23,19 @@ uint32_t* c_get_memory(uint32_t size) {
 
 int main() {
   pthread_jit_write_protect_np(0);
-  auto mem = c_get_memory(1024);
+  auto mem = c_get_memory(16384 * 10);
   printf("c_get_memory %d\n", (int)(intptr_t)mem);
   mem[0] = 12;
   mem[1] = 15;
   printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
   pthread_jit_write_protect_np(1);
-  sys_icache_invalidate(mem, 1024);
+  sys_icache_invalidate(mem, 16384 * 10);
+  printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
+  pthread_jit_write_protect_np(0);
+  auto fixedaddr = (uint32_t*)mmap(16384 * 5, 16384, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
+  printf("fixedaddr %d\n", (int)(intptr_t)fixedaddr);
+  pthread_jit_write_protect_np(1);
+  sys_icache_invalidate(mem, 16384 * 10);
+  mem[16384 * 5] = 12;
+  mem[16384 * 5 + 1] = 15;
 }
