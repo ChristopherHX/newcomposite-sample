9e75b7dc133bc493f45814136dab4f40486b42c7
ChristopherHX <christopher.homberger@web.de>
2023-04-05 23:09:43 +0200

    create the crash with less code

--- main.cpp
+++ main.cpp
@@ -5,42 +5,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-uint32_t* c_get_memory(uint32_t size) {
-    int prot = PROT_READ | PROT_WRITE | PROT_EXEC;
-    int flags = MAP_PRIVATE | MAP_ANONYMOUS | MAP_JIT;
-    int fd = -1;
-    int offset = 0;
-    uint32_t* addr = 0;
-
-    addr = (uint32_t*)mmap(0, size, prot, flags, fd, offset);
-    if (addr == MAP_FAILED){
-        printf("failure detected\n");
-        exit(-1);
-    }
-
-    return addr;
-}
-
 int main() {
-  pthread_jit_write_protect_np(0);
-  auto mem = c_get_memory(16384 * 100);
-  printf("c_get_memory %d\n", (int)(intptr_t)mem);
-  mem[0] = 12;
-  mem[1] = 15;
-  printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
-  pthread_jit_write_protect_np(1);
-  sys_icache_invalidate(mem, 16384 * 100);
-  printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
-  pthread_jit_write_protect_np(0);
-  auto fixedaddr = (uint32_t*)mmap((char*)mem + 16384 * 5, 16384 * 3, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS | MAP_FIXED, -1, 0);
-  printf("fixedaddr %d\n", (int)(intptr_t)fixedaddr);
-  if (fixedaddr == MAP_FAILED) {
-    return 1;
-  }
-  pthread_jit_write_protect_np(1);
-  sys_icache_invalidate(mem, 16384 * 100);
-  fixedaddr[0] = 12;
-  fixedaddr[1] = 15;
-  printf("mem[0] %d, mem[1] %d\n", (int)mem[0], (int)mem[1]);
-  printf("fixedaddr[0] %d, fixedaddr[1] %d\n", (int)fixedaddr[0], (int)fixedaddr[1]);
+  printf("initialize test\n");
+  void** tcb;
+  __asm__ volatile ("mrs %0, tpidr_el0" : "=r" (tcb));
+  // create a crash
+  auto pointer = tcb[0x28];
+  printf("finish test\n");
 }
